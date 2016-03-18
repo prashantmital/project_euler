6a5901a8b1e59a72ead29167604efd480c13126e
Prashant Mital <prashantmital@gmail.com>
2016-03-18 12:19:53 -0500

    updates

--- PEproblem345.py
+++ PEproblem345.py
@@ -1,57 +1,68 @@
 import numpy as np
 
-def compute_score(matrix):
-    _N = matrix.shape[0]
-    _row = np.zeros([_N, _N], dtype=float)
-    _col = np.zeros([_N, _N], dtype=float)
-    score_mtx = np.zeros([_N, _N], dtype=float)
-
-    for index in range(_N):
-        _row_max = matrix[index,:].max()
-        _col_max = matrix[:,index].max()
-        _row[index,:] = matrix[index,:]/_row_max
-        _col[:,index] = matrix[:,index]/_col_max
-
-    for index, (_r, _c) in enumerate(zip(_row, _col)):
-        score_mtx[index] = _r * _c
-
-    return score_mtx
-
-
-_raw = """7  53 183 439 863 497 383 563  79 973 287  63 343 169 583
-627 343 773 959 943 767 473 103 699 303 957 703 583 639 913
-447 283 463  29  23 487 463 993 119 883 327 493 423 159 743
-217 623   3 399 853 407 103 983  89 463 290 516 212 462 350
-960 376 682 962 300 780 486 502 912 800 250 346 172 812 350
-870 456 192 162 593 473 915  45 989 873 823 965 425 329 803
-973 965 905 919 133 673 665 235 509 613 673 815 165 992 326
-322 148 972 962 286 255 941 541 265 323 925 281 601  95 973
-445 721  11 525 473  65 511 164 138 672  18 428 154 448 848
-414 456 310 312 798 104 566 520 302 248 694 976 430 392 198
-184 829 373 181 631 101 969 613 840 740 778 458 284 760 390
-821 461 843 513  17 901 711 993 293 157 274  94 192 156 574
- 34 124   4 878 450 476 712 914 838 669 875 299 823 329 699
-815 559 813 459 522 788 168 586 966 232 308 833 251 631 107
-813 883 451 509 615  77 281 613 459 205 380 274 302  35 805"""
+_raw = """7  53 183 439 863
+497 383 563  79 973
+287  63 343 169 583
+627 343 773 959 943
+767 473 103 699 303"""
 
 _raw_data = _raw.split('\n')
 matrix = np.asarray([[float(ivar) for ivar in ovar.split()] for ovar in _raw_data])
 
 _N = matrix.shape[0]
-_sum = 0
+running_sum = 0
 
-while _N > 1:
-    _score = compute_weights(matrix).astype(int)
-    #ideally there are some ones in this matrix (makes the job easier)
-    _freqcol = _score.sum(0)
-    _freqrow = _score.sum(1)
-    _locate_ones = np.where(_score == 1)
+while _N >= 1:
+    # cost is a measure of the quantity NOT ADDED to our running sum
+    # essentially cost of an element is sum of the deficit caused in the running sum
+    # if that element were to be chosen (subtract the element from all
+    # entries on that row and col and add all these pieces to get cost)
+    #        therefore it is the COST of including that element
 
-    for _one in _locate_ones:
-        i, j = _one[0], _one[1]
-        if _freqrow[i] == 1 and _freqcol[j] == 1:
-            _sum += matrix[i,j]
-            matrix = np.delete(matrix, (i), axis=0)
-            matrix = np.delete(matrix, (j), axis=1)
-        else:
-            if _freqrow
+    cost_function = np.zeros(matrix.shape)
+    for row in range(_N):
+        for col in range(_N):
+            _score = 0
+            _element = matrix[row, col]
+            for loop_index in range(_N):
+                _score += (matrix[row, loop_index] - _element)
+                _score += (matrix[loop_index, col] - _element)
+            cost_function[row, col] = _score
+
+    # we want to minimize the cost
+    # BUT we want to minimize NETT COST
+    # NETTCOST is defined as the cost of selecting an element and NOT SELECTING
+    # the elements on that row and column. Subtract the combined sum of costs
+    # of row,column elements from the given element to arrive at this cost.
+
+    nettcost = np.zeros(matrix.shape)
+    for row in range(_N):
+        for col in range(_N):
+            _sum = 0
+            for loop_index in range(_N):
+                _sum += cost_function[row, loop_index]
+                _sum += cost_function[loop_index, col]
+            nettcost[row, col] = 3*cost_function[row, col] - _sum
+
+
+    target_index = np.where(nettcost == nettcost.min())
+    # now if multiple entries have same cost, select the entry with larger
+    # magnitude so as to maximize objective function
+
+    max_element = -1
+    print len(target_index[0])
+    for q in range(len(target_index[0])):
+        _i, _j = target_index[0][q], target_index[1][q]
+        if matrix[_i, _j] > max_element:
+            max_element = matrix[_i, _j]
+            i, j = _i, _j
+
+    print "optimal element: " + str(matrix[i, j])
+    running_sum += matrix[i, j]
+
+    matrix = np.delete(matrix, (i), axis=0)
+    matrix = np.delete(matrix, (j), axis=1)
+
+    _N = _N - 1
+
+print running_sum
